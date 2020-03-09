4d95f48571fe6666b099547726834f68a05667d0
Alex Larrabee <nekotree556@gmail.com>
2020-03-09 14:02:23 -0700

    updated display.js

--- book-assignment/src/components/Display.js
+++ book-assignment/src/components/Display.js
@@ -2,21 +2,50 @@ import React from 'react';
 
 class Display extends React.Component {
 
+    // showData = populateData = () => {
+
+    //     this.props.displayData.map(value => {
+
+    //         const dataArr = {
+    //             title: value.volumeInfo.title,
+    //             author: value.volumeInfo.authors,
+    //             description: value.volumeInfo.description
+
+    //         }
+    //         console.log(dataArr);
+    //     })
+
+    // }
+
 
     render() {
-        console.log(this.props.displayData)
+
 
         return (
 
             <div>
-                <section>
-                    <h2></h2>
-                    <p>Author</p>
-                    <p>Price</p>
-                    <p>Description</p>
-                    <img alt='book-cover-display' src="#" />
-                </section>
-            </div>
+
+
+                {this.props.displayData.map(value => {
+
+                    const dataArr = {
+                        title: value.volumeInfo.title,
+                        author: value.volumeInfo.authors,
+                        description: value.volumeInfo.description
+                    }
+                        < section >
+                        <h2>{this.props,.displayData.dataArr}</h2>
+                        <p>Author</p>
+                        <p>Price</p>
+                        <p>Description</p>
+                        <img alt='book-cover-display' src="#" />
+                    </section>
+
+                    })
+}
+                    
+                    
+            </div >
         )
     }
 
